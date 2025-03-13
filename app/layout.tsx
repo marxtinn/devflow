3ae55f3d497008da66228d5ea2c8b3bae07a9660
marxtinn <butarbutar.m@gmail.com>
2025-03-13 10:13:11 +0700

    ESLint & Prettier Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,31 @@
+import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+const geistMono = GeistMono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "DevFlow",
-    description: "A better version of Stack Overflow",
+  title: "DevFlow",
+  description: "A better version of Stack Overflow",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
 }
